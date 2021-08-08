08954ed20561de2e4ea5b04092822b70853f8a8a
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2021-08-08 11:53:03 +0530

    completed the project

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,29 +15,52 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        # In the code mentioned below we are just accepting the connection request.
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'Hello world!'
-            }
-        )
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         'type': 'tester_message',
+        #         'tester': 'Hello world!',
+        #     }
+        # )
 
 
-    async def tester_message(self, event):
-        tester = event['tester']
+    # async def tester_message(self, event):
+    #     tester = event['tester']
 
-        await self.send(text_date=json.dumps({
-            'tester':tester,
-        }))
+    #     await self.send(text_date=json.dumps({
+    #         'tester':tester,
+    #     }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'chatroom_message',
+                'message':message,
+                'username': username,
+            }
+        )
+
+    async def chatroom_message(self, event):
+        message = event['message']
+        username = event['username']
+        await self.send(text_data=json.dumps({
+            'message':message,
+            'username': username,
+        }))
     
     pass
 
